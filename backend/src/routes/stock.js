0378b7fc1cd8ce4b19a0de831fc1606f54a1a1cf
TitoGrine <tito999@gmail.com>
2020-11-29 16:43:01 +0000

    Merging problems resolved.

--- backend/src/routes/stock.js
+++ backend/src/routes/stock.js
@@ -1,35 +1,34 @@
-const express = require('express');
-const router = express.Router();
-const axios = require('axios');
-require('dotenv').config();
+var express = require("express");
+var router = express.Router();
+var axios = require("axios");
+require("dotenv").config();
 
-router.get('/allItems', function(req, res, next) {
+router.get("/", function (req, res, next) {
   const access_token = req.body.access_token;
-  console.log('Access token = ' + access_token);
-  if (!access_token) {
-    return res
-        .status(400)
-        .json({error: 'A valid access token was not provided.'});
-  }
 
-  const config = {
-    method: 'get',
+  if (!access_token)
+    return res
+      .status(400)
+      .json({ error: "A valid access token was not provided." });
+
+  var config = {
+    method: "get",
     url: `${process.env.JASMIN_URI}/api/${process.env.JASMIN_TENANT}/${process.env.JASMIN_ORGANIZATION}/materialscore/materialsitems`,
     headers: {
-      'Authorization': `Bearer ${access_token}`,
-      'Content-Type': 'application/json',
+      Authorization: `Bearer ${access_token}`,
+      "Content-Type": "application/json",
     },
   };
 
   axios(config)
-      .then(function(response) {
-        console.log(JSON.stringify(response.data));
-        res.send(response.data);
-      })
-      .catch(function(error) {
-        console.log(error);
-        return res.status(500).json({error});
-      });
+    .then(function (response) {
+      console.log(JSON.stringify(response.data));
+      res.send(response.data);
+    })
+    .catch(function (error) {
+      console.log(error);
+      return res.status(500).json({ error });
+    });
 });
 
 module.exports = router;
