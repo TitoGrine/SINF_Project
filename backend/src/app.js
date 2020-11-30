9067eee892dc18ceb9aed38759d8a3bed88f7c07
TitoGrine <tito999@gmail.com>
2020-11-30 00:10:38 +0000

    Fixed previous change that introduced a bug.

--- backend/src/app.js
+++ backend/src/app.js
@@ -27,7 +27,7 @@ app.use("/client", clientRouter);
 app.use("/supplier", supplierRouter);
 
 // catch 404 and forward to error handler
-app.use(function (next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
