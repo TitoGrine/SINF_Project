ba80e4e424d97d03a343a180a09a2a864ae3798a
João Martins <up201707311@fe.up.pt>
2020-12-04 23:44:44 +0000

    Added routes for picking wave

--- backend/src/routes/pickingWave.js
+++ backend/src/routes/pickingWave.js
@@ -2,68 +2,137 @@ require("dotenv").config();
 
 const express = require("express");
 const router = express.Router();
-const {clearQueryResults} = require("./utils/dbUtils");
+const { clearQueryResults } = require("../utils/dbUtils");
 
-const item = require("./models/item");
-const picking_wave = require("./picking_wave");
+const item = require("../models/item");
+const picking_wave = require("../models/picking_wave");
 
-//TODO: delete picking waves
-
-//TODO: get all picking waves
+// get all picking waves
 router.get("/", async (_, res) => {
-    
+
     const pWaves = clearQueryResults(await picking_wave.findAll());
-    
     return res.json(pWaves);
-    
+
 });
 
-//TODO: get picking wave items
-router.get("/:id/items", async (req, res) => {
-    
-    const id = req.params.ref;
+// get picking wave items
+router.get("/:ref/items", async (req, res) => {
+
+    const ref = req.params.ref;
     const items = clearQueryResults(await item.findAll({
         where: {
-            id_picking: id,
+            ref_picking: ref,
         },
     }));
-    
+
     return res.json(items);
 });
 
-//TODO: create picking wave
+// create picking wave
 router.post("/create", async (req, res) => {
-    
-    const {ref,date} = req.nody
+
+    const { ref, date } = req.body
     const pWave = await picking_wave.create(
         {
             ref,
-            date
+            final_date: date
         },
         {
-            fields: ["ref","final_date"]
+            fields: ["ref", "final_date"]
         }
     )
-    
+
     return res.status(201).json(pWave);
-    
+
 });
 
-//TODO: add/update picking wave items
-router.post("/:id/items", async (req, res) => {
-    const id = req.params.id;
+// add/update picking wave items
+router.post("/:ref/items", async (req, res) => {
+    const ref_picking = req.params.ref;
     const items = req.body.items;
+    console.log(items);
+    await Promise.all(
+        items.map(async ({ ref, product_name, quantity, order_ref, warehouse_zone }) => {
+            const result = await item.findOne({
+                where: {
+                    ref,
+                    product_name,
+                    quantity,
+                    order_ref,
+                    warehouse_zone,
+                    ref_picking: ref_picking,
+                }
+            });
 
-    await Promise.all()
-    const items = clearQueryResults(await item.findAll({
-        where: {
-            id_picking: id,
-        },
-    }));
-    
+            if (result) {
+                return result.update({
+                    quantity: result.quantity + quantity
+                });
+            } else {
+                return item.create(
+                    {
+                        ref,
+                        product_name,
+                        quantity,
+                        order_ref,
+                        warehouse_zone,
+                        ref_picking: ref_picking
+                    },
+                    {
+                        fields: [
+                            "ref",
+                            "product_name",
+                            "quantity",
+                            "order_ref",
+                            "warehouse_zone",
+                            "ref_picking"
+                        ]
+                    }
+                );
+            }
+        })
+    );
+
     return res.status(201).send();
 });
 
-//TODO: remove picking wave items
+//remove picking wave items
+router.delete("/:ref/item/:ref_item", async (req, res) => {
+    const { ref, ref_item } = req.params;
+
+    const i = await item.destroy({
+        where: {
+            ref: ref_item,
+            ref_picking: ref
+        }
+    });
+
+    if (i > 0)
+        return res.status(200).send();
+
+    return res.status(404).send();
+});
+
+// delete picking waves
+router.delete("/:ref", async (req, res) => {
+    const { ref } = req.params;
+
+    await item.destroy({
+        where: {
+            ref_picking: ref
+        }
+    })
+
+    const i = await picking_wave.destroy({
+        where: {
+            ref: ref
+        }
+    });
+
+    if (i > 0)
+        return res.status(200).send();
+
+    return res.status(404).send();
+});
 
 module.exports = router;
