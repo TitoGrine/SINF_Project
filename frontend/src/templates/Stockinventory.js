28fb42d606d6a340add16142d4337404d09ed396
Ana <anamargaridarl@gmail.com>
2020-12-13 14:02:43 +0000

    removed test parameters

--- frontend/src/templates/Stockinventory.js
+++ frontend/src/templates/Stockinventory.js
@@ -10,43 +10,43 @@ const useStyles = makeStyles(orderStyle);
 
 function Stockinventory() {
   const classes = useStyles();
-  // const location = useLocation();
-  // const data = location.state.params;
+  const location = useLocation();
+  const data = location.state.params;
 
-  let rows = [
-    {
-      id: 0,
-      documentId: "EF0.13",
-      storedquantity: "1",
-      orderedquantity: "15",
-      location: "A32",
-      productId: "123",
-    },
-    {
-      id: 1,
-      documentId: "EF0.13",
-      storedquantity: "1",
-      orderedquantity: "15",
-      location: "A45",
-      productId: "123",
-    },
-    {
-      id: 2,
-      documentId: "EF0.13",
-      storedquantity: "1",
-      orderedquantity: "15",
-      location: "A32",
-      productId: "123",
-    },
-    {
-      id: 3,
-      documentId: "EF0.13",
-      storedquantity: "1",
-      orderedquantity: "15",
-      location: "A32",
-      productId: "dftry",
-    },
-  ];
+  // let rows = [
+  //   {
+  //     id: 0,
+  //     documentId: "EF0.13",
+  //     storedquantity: "1",
+  //     orderedquantity: "15",
+  //     location: "A32",
+  //     productId: "123",
+  //   },
+  //   {
+  //     id: 1,
+  //     documentId: "EF0.13",
+  //     storedquantity: "1",
+  //     orderedquantity: "15",
+  //     location: "A45",
+  //     productId: "123",
+  //   },
+  //   {
+  //     id: 2,
+  //     documentId: "EF0.13",
+  //     storedquantity: "1",
+  //     orderedquantity: "15",
+  //     location: "A32",
+  //     productId: "123",
+  //   },
+  //   {
+  //     id: 3,
+  //     documentId: "EF0.13",
+  //     storedquantity: "1",
+  //     orderedquantity: "15",
+  //     location: "A32",
+  //     productId: "dftry",
+  //   },
+  // ];
 
   return (
     <div>
@@ -54,7 +54,7 @@ function Stockinventory() {
         <Grid item xs={12} sm={12} md={12} lg={12}>
           <h1 className={classes.title}>Stock Inventory</h1>
           <StockProvider>
-            <ListStockInventory rows={rows}></ListStockInventory>
+            <ListStockInventory rows={data}></ListStockInventory>
           </StockProvider>
         </Grid>
       </Grid>
